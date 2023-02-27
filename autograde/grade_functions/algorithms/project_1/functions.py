b67e536eb72e038fafcd59a79786c80ab04f9a85
JoshCu <Josh.Cu@gmail.com>
2023-02-27 13:52:10 -0500

    broken script out into multiple files

---
+++ autograde/grade_functions/algorithms/project_1/functions.py
@@ -0,0 +1,162 @@
+import asyncio
+import os
+import shutil
+import subprocess
+
+from datamodels import part1, part2
+from autograde.datamodels import test_result
+from autograde.file_formatter import find_file_path
+from autograde.workers import run_test
+
+
+async def grade_part_1(student: str) -> part1:
+
+    student_results = part1(student)
+
+    part1_path = os.path.join(os.getcwd(), student, 'part1')
+    test_files_folder = os.path.join(os.getcwd(), os.pardir, 'Project1_Grading_S2022', 'allFile2StudentFolder')
+
+    if os.path.exists(part1_path):
+        shutil.rmtree(part1_path)
+
+    shutil.copytree(test_files_folder, part1_path)
+
+    rsa_path = find_file_path('rsa435.cc', student)
+
+    if not rsa_path:
+        student_results.rsa_file_found.exit_code = 1
+        return student_results
+    else:
+        student_results.rsa_file_found.exit_code = 0
+
+    # copy students rsa435.cc into the folder
+    shutil.copy(rsa_path, part1_path)
+    # make all
+
+    # run make all and check for errors
+    if await run_test(student_results.rsa_file_compiles, ['make', 'all'], part1_path) != 0:
+        return student_results
+
+    # run .\rsa435.exe
+
+    binary_path = os.path.join(part1_path, 'rsa435')
+    if await run_test(student_results.rsa_file_runs, [binary_path], part1_path) != 0:
+        return student_results
+
+    # check for e_n.txt, d_n.txt p_q.txt
+    files = ['e_n.txt', 'd_n.txt', 'p_q.txt']
+    absolute_files = [os.path.join(part1_path, file) for file in files]
+
+    if not any([os.path.exists(file) for file in absolute_files]):
+        student_results.keys_file_found.exit_code = 1
+        return student_results
+    else:
+        student_results.keys_file_found.exit_code = 0
+
+    # make gradeing
+    if await run_test(student_results.grading_builds, ['make', 'grading'], part1_path) != 0:
+        return student_results
+
+    # run .\RSAPartIGrading -- get output
+    binary_path = os.path.join(part1_path, 'RSAPartIGrading')
+    if await run_test(student_results.grading_runs, [binary_path], part1_path) != 0:
+        return student_results
+    else:
+        student_results.grading_runs.result = f"{student_results.grading_runs.result.count('pass')}:6\n{student_results.grading_runs.result}"
+
+    return student_results
+
+
+async def grade_part_2(student: str) -> part2:
+
+    student_results = part2(student)
+
+    part2_path = os.path.join(os.getcwd(), student, 'part2')
+    test_files_folder = os.path.join(os.getcwd(), os.pardir, 'Project1_Grading_S2022', 'allFile2StudentFolder')
+    key_files_folder = os.path.join(os.getcwd(), os.pardir, 'Project1_Grading_S2022', '435WorkingKeys')
+
+    if os.path.exists(part2_path):
+        shutil.rmtree(part2_path)
+
+    shutil.copytree(test_files_folder, part2_path, dirs_exist_ok=True)
+    shutil.copytree(key_files_folder, part2_path, dirs_exist_ok=True)
+
+    message_path = find_file_path('messageDigest435.cpp', student)
+
+    if not message_path:
+        student_results.message_file_found.exit_code = 1
+        return student_results
+    else:
+        student_results.message_file_found.exit_code = 0
+
+    # copy students messageDigest435.cpp into the folder
+    shutil.copy(message_path, part2_path)
+
+    # run make all and check for errors
+    if await run_test(student_results.message_file_compiles, ['make', 'digest'], part2_path) != 0:
+        return student_results
+
+    if await run_test(test_result(), ['make', 'grading'], part2_path) != 0:
+        return student_results
+
+    test_files = ['one_line.txt', 'two_lines.txt', 'bible_part1.txt']
+    binary_path = os.path.join(part2_path, 'RSAPart2Grading')
+    sign_tasks = []
+    verify_tasks = []
+    for file in test_files:
+        sign_tasks.append(
+            asyncio.create_task(
+                run_test(student_results[f'{file[:-4]}_signed'],
+                         [binary_path, 's', os.path.join(part2_path, file)],
+                         part2_path)))
+
+    old_files = set(os.listdir(part2_path))
+    # run sign tasks
+    await asyncio.gather(*sign_tasks)
+    new_files = set(os.listdir(part2_path)) - old_files
+
+    with open(os.path.join(part2_path, 'modified.txt'), 'w') as f:
+        f.write("modified file test")
+
+    if len(new_files) == 0:
+        return student_results
+
+    # some students append .signature, some .signed, just get it straight from the files
+    signature_appenddix = None
+    signed_files = set()
+    for file in new_files:
+        if file.split('.')[-1].lower().startswith('sig'):
+            signature_appenddix = file.split('.')[-1]
+            signed_files.add(file)
+
+    if not signature_appenddix:
+        return student_results
+
+    for file in test_files:
+        verify_tasks.append(
+            asyncio.create_task(
+                run_test(
+                    student_results[f'{file[:-4]}_verified'],
+                    [binary_path, 'v', os.path.join(part2_path, file),
+                     os.path.join(part2_path, f'{os.path.join(part2_path, file)}.{signature_appenddix}')],
+                    part2_path)))
+        verify_tasks.append(
+            asyncio.create_task(
+                run_test(
+                    student_results[f'{file[:-4]}_modified'],
+                    [binary_path, 'v', os.path.join(part2_path, "modified.txt"),
+                     os.path.join(part2_path, f'{os.path.join(part2_path, file)}.{signature_appenddix}')],
+                    part2_path)))
+    signed_file_original_names = ['.'.join(file.split('.')[:-1]) for file in signed_files]
+    # students return codes are not consistent, so check if the file was signed
+    # if not, set the exit code to 1
+    for file in test_files:
+        if file not in signed_file_original_names:
+            student_results[f'{file[:-4]}_signed'].exit_code = 1
+            student_results[f'{file[:-4]}_verified'].exit_code = 1
+            student_results[f'{file[:-4]}_modified'].exit_code = 1
+
+    # run verify tasks
+    await asyncio.gather(*verify_tasks)
+
+    return student_results
---
+++ autograde/render.py
@@ -0,0 +1,67 @@
+import os
+from datamodels import test_result, exit_code
+
+
+def to_html(results, filename="results.html"):
+    # create an html file with the results in a table format
+    # one row per student, use the variable names in the dataclass as the column headers
+    # use the result and exit_code variables in the test_result dataclass as the values
+    # use the exit_code enum to determine the color of the cell
+    # green for success, light red for error, yellow for not attempted
+    results = sorted(results, key=lambda x: x.student_name)
+    headers = results[0].__annotations__.keys()
+    with open(filename, 'w') as f:
+        # import style sheet freeze.css
+        f.write('<link rel="stylesheet" href="../freeze.css">\n')
+        f.write('<html><body><table>\n')
+        f.write('<tr>')
+        for header in headers:
+            f.write(f'<th>{header}</th>')
+        f.write('</tr>\n')
+        for result in results:
+            f.write('<tr>')
+            for header in headers:
+                value = getattr(result, header)
+                if isinstance(value, test_result):
+                    # strip all non utf-8 characters
+                    value.result = value.result.encode('ascii', 'ignore').decode('ascii')
+                    # replace newlines with <br> so they are displayed correctly
+                    value.result = value.result.replace('\n', '<br>')
+                    if value.exit_code == exit_code.SUCCESS.value:
+                        f.write(
+                            f'<td style="background-color:lightgreen"><div class=scrollable>{value.result}</div></td>')
+                    elif value.exit_code == exit_code.ERROR.value:
+                        f.write(
+                            f'<td style="background-color:lightcoral"><div class=scrollable>{value.result}</div></td>')
+                    elif value.exit_code == exit_code.NOT_ATTEMPTED.value:
+                        f.write(
+                            f'<td style="background-color:lightyellow"><div class=scrollable>{value.result}</div></td>')
+                else:
+                    f.write(f'<td>{value}</td>')
+            f.write('</tr>\n')
+        f.write('</table></body></html>')
+
+
+def print_progress_bar(
+        iteration, total, prefix='', suffix='', decimals=1, length=os.get_terminal_size()[0], fill='█',
+        printEnd="\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r)
+    """
+    length -= len(prefix) + len(suffix) + 11
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end=printEnd)
+    # Print New Line on Complete
+    if iteration == total:
+        print()
